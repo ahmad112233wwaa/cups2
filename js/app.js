eecd79f609c6503014a770f42d562d576dcce9d2
ahmad112233wwaa <111597286+ahmad112233wwaa@users.noreply.github.com>
2022-12-10 22:01:10 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const menu=document.getElementById("menn");
+const action=document.getElementById("menn1");
+
+menu.addEventListener('click', () => {
+    hand();
+  });
+
+function hand(){
+  
+    menu.classList.toggle("is-active");
+    action.classList.toggle("is-active");
+
+}
